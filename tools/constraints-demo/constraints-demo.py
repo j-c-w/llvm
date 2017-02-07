4e85ec9492b67d298274e1c82391677539cabf30
Philip Ginsbach <philip.ginsbach@ed.ac.uk>
2017-02-07 11:13:13 -0600

    Added flat analysis output to demo gui.

--- tools/constraints-demo/constraints-demo.py
+++ tools/constraints-demo/constraints-demo.py
@@ -24,7 +24,8 @@ toplevel_box    = Gtk.HBox(homogeneous=True, spacing=10)
 left_box        = Gtk.VBox()
 menu_box        = Gtk.HBox()
 load_button     = Gtk.Button("load")
-analyze_button  = Gtk.Button("analyze")
+flatana_button  = Gtk.Button("analyze")
+analyze_button  = Gtk.Button("analyze pretty")
 hide_button     = Gtk.Button("hide")
 source_frame    = Gtk.Frame()
 source_box      = Gtk.ScrolledWindow()
@@ -48,6 +49,7 @@ toplevel_box.pack_start(left_box,        True,  True,  0)
 toplevel_box.pack_start(result_box,      True,  True,  0)
 left_box.    pack_start(menu_box,        False, True,  0)
 menu_box.    pack_start(load_button,     True,  True,  0)
+menu_box.    pack_start(flatana_button,  True,  True,  0)
 menu_box.    pack_start(analyze_button,  True,  True,  0)
 menu_box.    pack_start(hide_button,     True,  True,  0)
 left_box.    pack_start(source_frame,    True,  True,  0)
@@ -60,13 +62,11 @@ result_inner.pack_start(code_window3,    False, False, 5)
 
 sourcecode.     modify_font(Pango.FontDescription("Monospace 10"))
 load_button.    modify_font(Pango.FontDescription(     "Sans 10"))
+flatana_button. modify_font(Pango.FontDescription(     "Sans 10"))
 analyze_button. modify_font(Pango.FontDescription(     "Sans 10"))
 hide_button.    modify_font(Pango.FontDescription(     "Sans 10"))
-message_window1.modify_font(Pango.FontDescription("Monospace 10"))
 code_window1.   modify_font(Pango.FontDescription( "Monospace 8"))
-message_window2.modify_font(Pango.FontDescription("Monospace 10"))
 code_window2.   modify_font(Pango.FontDescription( "Monospace 8"))
-message_window3.modify_font(Pango.FontDescription("Monospace 10"))
 code_window3.   modify_font(Pango.FontDescription( "Monospace 8"))
 
 result_box.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.ALWAYS)
@@ -108,22 +108,26 @@ def map_IR_to_C_line(instruction):
     has_hit = 0
     with open(LLVM_FILENAME, 'r') as temp_file:
         for line in temp_file:
-            if has_hit == 0 and line.rstrip() == instruction:
+            if has_hit == 0 and line.strip() == instruction:
                 has_hit = 1
             if has_hit == 1 and "!" in line:
-                instruction = line.rstrip().split("!")[-1]
+                instruction = line.strip().split("!")[-1]
                 has_hit = 2
             if has_hit == 2 and line[:1] == "!" and line[1:][:len(instruction)] == instruction:
                 return int(line.split('(')[1].split(')')[0].split("line: ")[1].split(",")[0])
 
     return None
 
-def set_output(part1 = "", part2 = "", part3 = "", part4 = "", part5 = "", part6 = ""):
+def set_output(part1 = "", part2 = "", part3 = "", part4 = "", part5 = "", part6 = "", downscale = 0):
+
+    message_window1.modify_font(Pango.FontDescription("Monospace "+str(10-downscale)))
     message_window1.set_text(part1)
     code_window1.get_buffer().set_text(part2)
+    message_window2.modify_font(Pango.FontDescription("Monospace "+str(10-downscale)))
     message_window2.set_text(part3)
     code_window2.get_buffer().set_text(part4)
     message_window3.set_text(part5)
+    message_window3.modify_font(Pango.FontDescription("Monospace "+str(10-downscale)))
     code_window3.get_buffer().set_text(part6)
 
     if part2: code_window1.show()
@@ -159,6 +163,22 @@ def on_analyze_clicked(widget):
     already_running_analysis = True
     already_running_analysis_lock.release()
 
+
+def on_flatana_clicked(widget):
+    global already_running_analysis, already_running_analysis_lock
+
+    source_code = sourcecode.get_buffer().get_text(sourcecode.get_buffer().get_bounds()[0],
+                                                   sourcecode.get_buffer().get_bounds()[1], False)
+    with open(SOURCE_FILENAME, 'w') as temp_file:
+        temp_file.write(source_code+"\n")
+    
+    already_running_analysis_lock.acquire()
+    if not already_running_analysis:
+        set_output("Running clang and SMT solver.")
+        threading.Thread(target=flatwait_thread, args=(source_code,)).start()
+    already_running_analysis = True
+    already_running_analysis_lock.release()
+
 def extract_type_from_llmv_value(code):
     possible_tokens = [t for t in code.split(",")[0].split(" = ")[-1].split(" ") if t != ""]
 
@@ -176,24 +196,29 @@ def generate_reduction(solutions):
 
     iter_type  = ""
     iter_begin = ""
+    iter_step  = ""
     iter_end   = ""
 
     for solution in solutions:
         iter_type = extract_type_from_llmv_value(solution["iterator"])
         if len(solution["iter_begin"].strip().split(' ')) == 2:
             iter_begin = "="+solution["iter_begin"].strip().split(' ')[1]
+        if len(solution["iter_step"].strip().split(' ')) == 2:
+            iter_step = "+="+solution["iter_step"].strip().split(' ')[1]
         if len(solution["iter_end"].strip().split(' ')) == 2:
             iter_end = "="+solution["iter_end"].strip().split(' ')[1]
 
     if not iter_end:
         iter_begin = ""
+    if iter_step == "+=1" or not iter_step:
+        iter_step = "++"
 
     part1 = ("void reduce_par(out_t* out")
     part2 = (                          ",\n"
             +"                "+iter_type+" begin"+iter_begin+", "+iter_type+" end"+iter_end+")\n"
             +"{\n"
             +" if(/*...*/) { //sequential execution\n"
-            +"  for("+iter_type+" i = begin; i < end; i++)\n"
+            +"  for("+iter_type+" i = begin; i < end; i"+iter_step+")\n"
             +"   op(out")
     part3 = (         ",begin,end);\n"
             +" }\n"
@@ -286,13 +311,45 @@ def wait_thread(source_code):
     already_running_analysis = False
     already_running_analysis_lock.release()
 
+
+def flatwait_thread(source_code):
+    global already_running_analysis, already_running_analysis_lock
+
+    process = subprocess.Popen([WRAPPER_SCRIPT, CLANGPP_EXECUTABLE, "-std=c++14",  "-S", "-emit-llvm",
+                                "-o", TARGET_FILENAME, "-gline-tables-only", SOURCE_FILENAME], stderr=subprocess.PIPE)
+    process.wait()
+
+    stderr_result = process.stderr.read().decode("utf8")
+
+    Gdk.threads_enter()
+    if stderr_result:
+        set_output(stderr_result)
+
+    else:
+        stdout_result = open("replace-report.txt").read().decode("utf8")
+
+        for loop in [p.split("END LOOP\n")[0] for p in stdout_result.split("BEGIN LOOP\n")[1:]]:
+            scalars  = [eval(p.split("END SCALAR REDUCTION\n")[0]) for p in loop.split("BEGIN SCALAR REDUCTION\n")[1:]]
+            histos   = [eval(p.split("END HISTOGRAM\n")[0])        for p in loop.split("BEGIN HISTOGRAM\n")[1:]]
+
+            set_output(loop.split("BEGIN OPERATOR\n")[0], downscale=3)
+            break
+        else:
+            set_output("No reductions were identified.")
+
+    Gdk.threads_leave()
+    already_running_analysis_lock.acquire()
+    already_running_analysis = False
+    already_running_analysis_lock.release()
+
 def on_hide_clicked(widget):
     set_output()
 
-window        .connect("delete-event", Gtk.main_quit)
-load_button   .connect("clicked",      on_open_clicked)
+window.        connect("delete-event", Gtk.main_quit)
+load_button.   connect("clicked",      on_open_clicked)
 analyze_button.connect("clicked",      on_analyze_clicked)
-hide_button   .connect("clicked",      on_hide_clicked)
+flatana_button.connect("clicked",      on_flatana_clicked)
+hide_button.   connect("clicked",      on_hide_clicked)
 
 Gdk.threads_init()
 window.show_all()
