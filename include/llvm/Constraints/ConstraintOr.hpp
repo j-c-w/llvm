95ecbf8d3c2eff0b3eeafc4477bd0e52e9658b2b
Philip Ginsbach <philip.ginsbach@ed.ac.uk>
2017-02-13 14:17:52 +0000

    Proper use of SFINAE in object construction.

--- include/llvm/Constraints/ConstraintOr.hpp
+++ include/llvm/Constraints/ConstraintOr.hpp
@@ -12,7 +12,7 @@ class ConstraintOr : public Constraint<LabelType,ValueType>
 {
 public:
     template<typename ... Types>
-    ConstraintOr(Types ... constraints) { add_constraint(constraints ...); }
+    ConstraintOr(Types ... constraints) { add_constraints(constraints ...); }
 
     std::vector<std::pair<LabelType,std::unique_ptr<Specialized<ValueType>>>> get_specializations() const final;
 
@@ -24,7 +24,8 @@ private:
     };
 
     class SpecializedOr : public Specialized<ValueType> //This crashed when some branches don't use all parameters!
-                                                        //Not sure whether the commen still applies? Need to investigate!
+                                                        //Update: Not sure whether the comment still applies.
+                                                        //Need to investigate!
     {
     public:
         SpecializedOr(std::vector<std::unique_ptr<Specialized<ValueType>>> c, std::shared_ptr<SharedData> c2);
@@ -43,31 +44,49 @@ private:
         unsigned                                                 temp_result;
     };
 
-    void add_constraint() { }
+    void add_constraints() { }
 
     template<typename TFirst, typename ... TRest> 
-    void add_constraint(TFirst first, TRest ... rest);
+    void add_constraints(TFirst first, TRest ... rest);
+
+    template<typename Type>
+    void add_constraint(Type constraint,
+            typename std::enable_if<std::is_base_of<ConstraintOr<LabelType,ValueType>,Type>::value>::type* = nullptr);
+
+    template<typename Type>
+    void add_constraint(Type first,
+            typename std::enable_if<std::is_base_of<Constraint<LabelType,ValueType>,Type>::value>::type* = nullptr,
+            typename std::enable_if<!std::is_base_of<ConstraintOr<LabelType,ValueType>,Type>::value>::type* = nullptr);
 
     std::vector<std::shared_ptr<Constraint<LabelType,ValueType>>> constraints;
 };
 
 template<typename LabelType, typename ValueType>
 template<typename TFirst, typename ... TRest> 
-void ConstraintOr<LabelType,ValueType>::add_constraint(TFirst first, TRest ... rest)
+void ConstraintOr<LabelType,ValueType>::add_constraints(TFirst first, TRest ... rest)
 {
-    if(std::is_base_of<ConstraintOr<LabelType,ValueType>, TFirst>::value)
-    {
-        for(auto& constraint : ((ConstraintOr<LabelType,ValueType>&)(first)).constraints)
-        {
-            constraints.emplace_back(std::move(constraint));
-        }
-    }
-    else
-    {
-        constraints.push_back(std::shared_ptr<Constraint<LabelType,ValueType>>(new TFirst(first)));
-    }
+    add_constraint(first);
+    add_constraints(rest...);
+}
 
-    add_constraint(rest...);
+template<typename LabelType, typename ValueType>
+template<typename Type>
+void ConstraintOr<LabelType,ValueType>::add_constraint(Type constraint,
+            typename std::enable_if<std::is_base_of<ConstraintOr<LabelType,ValueType>,Type>::value>::type*)
+{
+    for(auto& constraint : constraint.constraints)
+    {
+        constraints.emplace_back(std::move(constraint));
+    }
+}
+
+template<typename LabelType, typename ValueType>
+template<typename Type>
+void ConstraintOr<LabelType,ValueType>::add_constraint(Type constraint,
+            typename std::enable_if<std::is_base_of<Constraint<LabelType,ValueType>,Type>::value>::type*,
+            typename std::enable_if<!std::is_base_of<ConstraintOr<LabelType,ValueType>,Type>::value>::type*)
+{
+    constraints.push_back(std::shared_ptr<Constraint<LabelType,ValueType>>(new Type(constraint)));
 }
 
 #endif
