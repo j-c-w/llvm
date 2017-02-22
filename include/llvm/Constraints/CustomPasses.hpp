f9f8b2080ef8d22ee328fdd5977af14ef94f5d5f
Philip Ginsbach <philip.ginsbach@ed.ac.uk>
2017-02-22 16:39:02 +0000

    Moved the definition of RemovePHIPass to header file.

--- include/llvm/Constraints/CustomPasses.hpp
+++ include/llvm/Constraints/CustomPasses.hpp
@@ -3,15 +3,17 @@
 
 namespace llvm {
 
+class FunctionPass;
 class ModulePass;
 
 /* FlattenPass transforms all array accesses to be flat accesses to bare pointers.
    PreprocessorPass implements some additional peephole optimisations.
    ReplacerPass is the central part of this project, it uses the SMT based constraint solver to detect computational
    idioms in LLVM IR code. */
-ModulePass *createFlattenPass();
-ModulePass *createPreprocessorPass();
-ModulePass *createReplacerPass();
+FunctionPass* createRemovePHIPass();
+ModulePass*   createFlattenPass();
+ModulePass*   createPreprocessorPass();
+ModulePass*   createReplacerPass();
 }
 
 #endif
