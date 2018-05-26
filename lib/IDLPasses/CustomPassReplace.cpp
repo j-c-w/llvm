ab69e833cc6b273273d2fad16fa75a25d4b2f908
Philip Ginsbach <philip.ginsbach@ed.ac.uk>
2018-05-26 18:31:51 +0100

    Minor fix after the merge; refined the output of replacer pass.

--- lib/IDLPasses/CustomPassReplace.cpp
+++ lib/IDLPasses/CustomPassReplace.cpp
@@ -1,19 +1,15 @@
 #include "llvm/IDLPasses/CustomPasses.hpp"
 #include "llvm/IDLParser/Solution.hpp"
-#include "llvm/IDLPasses/Transforms.hpp"
 #include "llvm/IR/ModuleSlotTracker.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/DebugLoc.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
-#include <algorithm>
-#include <iostream>
 #include <sstream>
 #include <fstream>
 #include <string>
 #include <vector>
-#include <map>
 
 using namespace llvm;
 
@@ -22,63 +18,10 @@ class ResearchReplacer : public ModulePass
 public:
     static char ID;
 
-    ResearchReplacer() : ModulePass(ID),
-                         loop_constraint_specs{{"GEMM",       GenerateAnalysis("GEMM")},
-                                               {"SPMV",       GenerateAnalysis("SPMV")}} { }
+    ResearchReplacer() : ModulePass(ID) { }
 
     bool runOnModule(Module& module) override;
-
-private:
-    std::vector<std::pair<std::string,std::vector<Solution>(*)(Function&,unsigned)>> loop_constraint_specs;
 };
-
-struct SolutionCluster
-{
-    Instruction*                                precursor;
-    Instruction*                                begin;
-    Instruction*                                end;
-    Instruction*                                successor;
-    std::map<std::string,std::vector<Solution>> solutions;
-};
-
-// This function applies some postprocessing to bundle together reductions.
-// The proper way would be to encode it in a new constraint specification "composed reduction" but this will do for now.
-std::vector<SolutionCluster> cluster_solutions(std::vector<std::pair<std::string,std::vector<Solution>>> inputs)
-{
-    std::vector<SolutionCluster> result;
-
-    for(auto& input : inputs)
-    {
-        for(const auto& solution : input.second)
-        {
-            Value* precursor_value      = (Value*)solution.get("precursor");
-            Value* begin_value          = (Value*)solution.get("begin");
-            Value* end_value            = (Value*)solution.get("end");
-            Value* successor_value      = (Value*)solution.get("successor");
-
-            if(precursor_value == nullptr)
-                precursor_value = (Value*)solution.get("store_new_next");
-
-            Instruction* precursor      = dyn_cast_or_null<Instruction>(precursor_value);
-            Instruction* begin          = dyn_cast_or_null<Instruction>(begin_value);
-            Instruction* end            = dyn_cast_or_null<Instruction>(end_value);
-            Instruction* successor      = dyn_cast_or_null<Instruction>(successor_value);
-
-            
-
-            auto find_it = std::find_if(result.begin(), result.end(), [=](const SolutionCluster& cluster)
-                                        { return cluster.begin == begin && cluster.successor == successor; });
-
-            if(find_it == result.end())
-                result.push_back(SolutionCluster{precursor, begin, end, successor,
-                                                 {{input.first, {std::move(solution)}}}});
-            else
-                find_it->solutions[input.first].push_back(std::move(solution));
-        }
-    }
-
-    return result;
-}
 
 void replace_spmv(Function& function, Solution solution)
 {
@@ -100,11 +43,7 @@ void replace_spmv(Function& function, Solution solution)
                                      {ov->getType(), a->getType(), iv->getType(),
                                      rowstr->getType(), colidx->getType(), rows->getType()}, false);
 
-
         Constant* func = function.getParent()->getOrInsertFunction("spmv_harness", func_type);
-
-    //    BasicBlock::iterator iter(precursor);
-    //    SmartIRBuilder builder(comparison->getParent(), iter);
 
         CallInst::Create(func, {ov, a, iv, rowstr, colidx, rows}, "", precursor);
     }
@@ -122,66 +61,43 @@ bool ResearchReplacer::runOnModule(Module& module)
     std::stringstream sstr;
     sstr<<"replace-report-"<<filename<<".json";
     std::ofstream ofs(sstr.str().c_str());
-    ofs<<"{\n  \"filename\": \""<<(std::string)module.getName()<<"\",\n  \"loops\": [";
+    ofs<<"{\"filename\": \""<<(std::string)module.getName()<<"\",\n \"detected\": [";
 
     char first_hit1 = true;
     for(Function& function : module.getFunctionList())
     {
-        if(!function.isDeclaration())
+        for(auto& idiom : std::vector<std::string>{"GEMM", "SPMV"})
         {
-            std::vector<std::pair<std::string,std::vector<Solution>>> raw_solutions;
-            for(const auto& spec : loop_constraint_specs)
-                raw_solutions.push_back({spec.first, spec.second(function,100)});
-
-            auto clustered_solutions = cluster_solutions(std::move(raw_solutions));
-
-            if(clustered_solutions.empty())
-                continue;
-
-            for(unsigned i = 0; i < clustered_solutions.size(); i++)
+            for(auto& solution : GenerateAnalysis(idiom)(function, 100))
             {
                 unsigned line_begin = 999;
 
-                if(clustered_solutions[i].precursor && clustered_solutions[i].precursor->getDebugLoc())
-                    line_begin = clustered_solutions[i].precursor->getDebugLoc().getLine();
+                if(auto precursor = dyn_cast<Instruction>((Value*)solution["precursor"]))
+                    if(auto& debugloc = precursor->getDebugLoc())
+                        line_begin = debugloc.getLine();
 
                 ofs<<(first_hit1?"\n":",\n");
-                ofs<<"    {\n      \"function\": \""<<(std::string)function.getName()<<"\",\n";
-                ofs<<"      \"line\": "<<line_begin<<",\n";
-                ofs<<"      \"idioms\": [\n";
-
-                char first_hit2 = true;
-                for(const auto& spec : loop_constraint_specs)
+                ofs<<"  {\"function\": \""<<(std::string)function.getName()<<"\",\n";
+                ofs<<"   \"line\": "<<line_begin<<",\n";
+                ofs<<"   \"type\": \""<<idiom<<"\",\n";
+                ofs<<"   \"solution\":\n    ";
+                for(char c : solution.prune().print_json(slot_tracker))
                 {
-                    for(auto& solution : clustered_solutions[i].solutions[spec.first])
-                    {
-                        ofs<<(first_hit2?"\n":",\n");
-                        ofs<<"    {\n      \"type\": \""<<spec.first<<"\",\n";
-                        ofs<<"      \"solution\":\n        ";
-                        for(char c : solution.prune().print_json(slot_tracker))
-                        {
-                            ofs.put(c);
-                            if(c == '\n') ofs<<"        ";
-                        }
-                        ofs<<"\n    }";
-                        first_hit2 = false;
-                    }
+                    ofs.put(c);
+                    if(c == '\n') ofs<<"    ";
                 }
+                ofs<<"\n  }";
+                first_hit1 = false;
 
-                for(auto& solution : clustered_solutions[i].solutions["SPMV"])
+                if(idiom == "SPMV")
                 {
                     replace_spmv(function, solution);
                 }
-
-                ofs<<"]";
-                ofs<<"\n    }";
-                first_hit1 = false;
             }
         }
     }
 
     ofs<<"]\n}\n";
-
     return false;
 }
 
