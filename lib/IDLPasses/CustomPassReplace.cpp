0c8871c3dedc52fdba5c6f61cd85e52452c9d16a
Philip Ginsbach <philip.ginsbach@ed.ac.uk>
2018-10-03 16:47:38 +0100

    Enhanced reduction loop constraint description.

--- lib/IDLPasses/CustomPassReplace.cpp
+++ lib/IDLPasses/CustomPassReplace.cpp
@@ -87,10 +87,10 @@ class ResearchReplacer : public ResearchReplacerBase
 {
 public:
     ResearchReplacer() : ResearchReplacerBase({
-    {"GEMM",             [](const Solution& s)->Value*{ return s["for"][0]["comparison"]; }, nullptr}/*,
+    {"GEMM",             [](const Solution& s)->Value*{ return s["for"][0]["comparison"]; }, nullptr},
     {"SPMV",             [](const Solution& s)->Value*{ return s["comparison"]; }, nullptr},
-    {"ComplexReduction", [](const Solution& s)->Value*{ return s["comparison"]; }, nullptr},
-    {"Stencil",          [](const Solution& s)->Value*{ return s["for"][0]["comparison"]; }, nullptr}*/}) { }
+//    {"ComplexReduction", [](const Solution& s)->Value*{ return s["comparison"]; }, nullptr},
+    {"Stencil",          [](const Solution& s)->Value*{ return s["for"][0]["comparison"]; }, nullptr}}) { }
 };
 
 static RegisterPass<ResearchReplacer> X("research-replacer", "Research replacer", false, false);
