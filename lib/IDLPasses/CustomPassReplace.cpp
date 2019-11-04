358a16e0a43c53a0c7b746ca16fc01769974def9
Philip Ginsbach <philip.ginsbach@ed.ac.uk>
2019-11-04 17:15:06 +0000

    Worked on the reduction idioms

--- lib/IDLPasses/CustomPassReplace.cpp
+++ lib/IDLPasses/CustomPassReplace.cpp
@@ -97,9 +97,7 @@ class ResearchReplacer : public ResearchReplacerBase
 {
 public:
     ResearchReplacer() : ResearchReplacerBase({
-    {"GEMM",             [](const Solution& s)->Value*{ return s["for"][0]["comparison"]; }, nullptr},
-    {"SPMV",             [](const Solution& s)->Value*{ return s["comparison"]; }, nullptr},
-    {"ComplexReduction", [](const Solution& s)->Value*{ return s["comparison"]; }, nullptr}}) { }
+   {"ComplexReduction", [](const Solution& s)->Value*{ return s["loop"]["comparison"]; }, nullptr}}) { }
 };
 
 static RegisterPass<ResearchReplacer> X("research-replacer", "Research replacer", false, false);
