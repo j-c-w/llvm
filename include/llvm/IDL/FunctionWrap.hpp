32f5f69f23b88236c9ebd662c202ee942a3ff6c5
Philip Ginsbach <philip.ginsbach@ed.ac.uk>
2020-01-07 14:51:47 +0000

    put some preparatory stuff in FunctionWrap

--- include/llvm/IDL/FunctionWrap.hpp
+++ include/llvm/IDL/FunctionWrap.hpp
@@ -1,5 +1,6 @@
 #ifndef _FUNCTION_WRAP_HH_
 #define _FUNCTION_WRAP_HH_
+#include <unordered_map>
 #include <vector>
 
 namespace llvm {
@@ -7,11 +8,22 @@ class Function;
 class Value;
 }
 
+class Tree : public std::vector<std::pair<unsigned,Tree>> {};
+
 /* This class is used as a wrapper around llvm::Function. */
 class FunctionWrap : public std::vector<llvm::Value*>
 {
 public:
     FunctionWrap(llvm::Function& llvm_function);
+
+    const Tree& derive_set(Tree(*func)(FunctionWrap&,const llvm::Function&)) {
+        auto find_it = cache.find(func);
+        if(find_it != cache.end())
+            return find_it->second;
+
+        return cache.insert({func, func(*this, function)}).first->second;
+    }
+
 
     llvm::Function& function;
 
@@ -31,6 +43,9 @@ public:
     std::vector<std::vector<unsigned>>              rblocks;
 
     static double stopped_time;
+
+private:
+    std::unordered_map<Tree(*)(FunctionWrap&,const llvm::Function&),Tree> cache;
 };
 
 #endif
