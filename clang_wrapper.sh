e3e7d462d660fe03c586708808149165e4468f21
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2020-01-27 18:09:27 +0000

    Remove the clang wrapper from the llvm repo (it's in clang repo now)

--- clang_wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Try running idl clang, then fallback to normal clang.
-
-execdir=$(dirname $0)
-
-idl_clang="$execdir/idlclang"
-
-exec $idl_clang "$@" || (exec $idl_clang "$@" --fno-idl)
